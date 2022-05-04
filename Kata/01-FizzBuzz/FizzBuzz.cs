5af952cd1f866b789af39510134aef0f596d6b9e
Pchr-7464 <77829215+Pchr-7464@users.noreply.github.com>
2022-05-04 17:11:48 +0700

    testcase3

--- Kata/01-FizzBuzz/FizzBuzz.cs
+++ Kata/01-FizzBuzz/FizzBuzz.cs
@@ -4,7 +4,9 @@ public class FizzBuzz
 {
     public string GetFizzBuzz(int number)
     {
-        if (number == 2 )
+        if (number % 3 == 0)
+            return "Fizz";
+        else if (number == 2 )
             return "2";
         
         return "1";
